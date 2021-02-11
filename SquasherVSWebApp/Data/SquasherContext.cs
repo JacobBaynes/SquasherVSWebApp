d0e8fe64c25a84533f4959291d5487c7a7d54896
Jacob Baynes <jacob.baynes@icloud.com>
2021-02-11 18:31:55 -0500

    Created DbContext file SquasherContext.cs

---
+++ SquasherVSWebApp/Data/SquasherContext.cs
@@ -0,0 +1,20 @@
+﻿using SquasherVSWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SquasherVSWebApp.Data
+{
+    public class SquasherContext : DbContext
+    {
+        public SquasherContext(DbContextOptions<SquasherContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Bug> Bugs { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Squasher> Squashers { get; set; }
+        public DbSet<ProjectBugAssignment> ProjectBugAssignments { get; set; }
+        public DbSet<ProjectSquasherAssignment> ProjectSquasherAssignments { get; set; }
+        public DbSet<SquasherBugAssignment> SquasherBugAssignments { get; set; }
+
+    }
+}
