916b4e21cf375fa8adee3e1560557fc709e8bf71
Jacob Baynes <jacob.baynes@icloud.com>
2021-02-11 17:45:09 -0500

    Added new navigation tabs to _Layout.cshtml

---
+++ SquasherVSWebApp/Models/Bug.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SquasherVSWebApp.Models
+{
+    public class Bug
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Severity { get; set; }
+        public ICollection<ProjectAssignment> ProjectAssignments { get; set; }
+        public ICollection<SquasherAssignment> SquasherAssignments { get; set; }
+
+    }
+}
