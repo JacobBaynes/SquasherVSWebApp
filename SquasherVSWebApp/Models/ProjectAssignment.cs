1eeed0df42b998085da3ad52a7222563e312dbf4
Jacob Baynes <jacob.baynes@icloud.com>
2021-02-11 17:55:28 -0500

    Added ProjectAssignment Model

---
+++ SquasherVSWebApp/Models/ProjectAssignment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SquasherVSWebApp.Models
+{
+    public class ProjectAssignment
+    {
+        public int ID { get; set; }
+        public int ProjectID { get; set; }
+        public int BugID { get; set; }
+
+        public Bug Bug { get; set; }
+        public Project Project { get; set; }
+
+    }
+}
